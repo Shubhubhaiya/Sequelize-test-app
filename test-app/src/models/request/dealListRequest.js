dca4a9872f2a298b78a67b0a57363d6f41bc44d6
Shubhdeep Verma <shubhdeepverma@Shubhdeeps-MacBook-Air.local>
2024-09-26 15:29:37 +0530

    fix schema for list

--- test-app/src/models/request/dealListRequest.js
+++ test-app/src/models/request/dealListRequest.js
@@ -2,17 +2,18 @@ const Joi = require('joi');
 
 // Define the validation schema for filters
 const dealListRequest = Joi.object({
-  name: Joi.string().trim().optional().messages({
+  name: Joi.string().trim().allow(null).optional().messages({
     'string.base': 'Name must be a string'
   }),
 
   therapeuticArea: Joi.array()
     .items(
-      Joi.number().integer().strict().messages({
+      Joi.number().integer().strict().allow(null).messages({
         'number.base': 'Therapeutic Area ID must be a number',
         'number.integer': 'Therapeutic Area ID must be an integer'
       })
     )
+    .allow(null)
     .optional()
     .messages({
       'array.base': 'Therapeutic Area must be an array of numbers'
@@ -20,25 +21,26 @@ const dealListRequest = Joi.object({
 
   stage: Joi.array()
     .items(
-      Joi.number().integer().strict().messages({
+      Joi.number().integer().strict().allow(null).messages({
         'number.base': 'Stage ID must be a number',
         'number.integer': 'Stage ID must be an integer'
       })
     )
+    .allow(null)
     .optional()
     .messages({
       'array.base': 'Stage must be an array of numbers'
     }),
 
-  modifiedBy: Joi.string().trim().optional().messages({
+  modifiedBy: Joi.string().trim().allow(null).optional().messages({
     'string.base': 'Modified By must be a string'
   }),
 
-  modifiedAt: Joi.date().optional().messages({
+  modifiedAt: Joi.date().allow(null).optional().messages({
     'date.base': 'Modified At must be a valid date'
   }),
 
-  dealLead: Joi.string().trim().optional().messages({
+  dealLead: Joi.string().trim().allow(null).optional().messages({
     'string.base': 'Deal Lead must be a string'
   })
 });
